11b69963853c4f969b4e88a5cdd223c746285b15
Nathan Comiso <84970206+Exor10@users.noreply.github.com>
2026-04-21 13:03:13 +0800

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  API_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE'
+  API_URL: 'https://script.google.com/macros/s/AKfycbydFCdDntruzp6b8XKLA7k7r2lXZEULT_4a8oviXfZe53mz2xSzMPjfYzn4lcTRwujOoQ/exec'
 };
